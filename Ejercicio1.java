b305ab2f8ea6b64fb8e36f7e9fd091c97f49d161
blancaamaldonadoo <159050395+blancaamaldonadoo@users.noreply.github.com>
2024-02-23 12:57:28 +0000

    terminado

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -6,21 +6,44 @@ el número de segundos que tiene un minuto. Emplear las variables que ocupen el 
  Realizar preferiblemente mediante una función que reciba como parámetros el número de años, meses
  y días y devuelva el número de segundos de los días, meses y años introducidos.*/
 
- public static void main(){
-  int dias_ano=365;
-  int horas_dia=24;
-  int minutos_hora=60;
-  int segundos_minuto=60;
+import java.util.Scanner;
 
-  int segundos_Ano= dias_ano*horas_dia*minutos_hora*segundos_minuto;
+public class Ejercicio1{
 
-  int segsAno(int anos, int meses, int dias){
+  public static int segsAno(int anos,int meses,int dias){
+    int dias_ano=365;
+    int horas_dia=24;
+    int minutos_hora=60;
+    int segundos_minuto=60;
 
     int segstotales= (anos*dias_ano*horas_dia*minutos_hora*segundos_minuto)+(meses*31*horas_dia*minutos_hora*segundos_minuto)
-    +(dias*horas_dia*minutos_hora*segundos_minuto)
+    +(dias*horas_dia*minutos_hora*segundos_minuto);
 
     return segstotales;
   }
+
+
+ public static void main(String[] args){
+
+  Scanner scanner= new Scanner(System.in);
+
+  //Para que introduzca el numero de años,meses y días, utilizamos System.in
+
+  System.out.println("Introduzca el número de años");
+  int anosintrod= scanner.nextInt();
   
+
+  System.out.println("Introduzca el número de meses");
+  int mesesintrod= scanner.nextInt();
+
+
+  System.out.println("Introduzca el número de días");
+  int diasintrod= scanner.nextInt();
+
+  int solucion= segsAno(anosintrod, mesesintrod, diasintrod);
+  System.out.println("El número de segundos para los " + anosintrod+ " años, " + mesesintrod+ " meses y "
+  + diasintrod + " dias, son: " + solucion + " segundos");
+
  }
 
+ }
