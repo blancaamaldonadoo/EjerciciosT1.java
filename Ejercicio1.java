218532603cfbbb5fe46a8f97d434f3118064e9ff
blancaamaldonadoo <159050395+blancaamaldonadoo@users.noreply.github.com>
2024-02-23 13:00:58 +0100

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,10 +1,18 @@
 /*Escribir un programa que defina variables que representen el número de días de un año, 
-el número de horas que tiene un día, el número deminutos que tiene una hora y 
-el número de segundos que tiene un minuto.Emplear las variables que ocupen el mínimo espacio
+el número de horas que tiene un día, el número de minutos que tiene una hora y 
+el número de segundos que tiene un minuto. Emplear las variables que ocupen el mínimo espacio
  de memoria posible. A continuación, calcular el número de segundos que tiene un año 
  y almacenar el valor del cálculo en otra variable. 
  Realizar preferiblemente mediante unafunción que reciba como parámetros el número de años, meses
  y días ydevuelva el número de segundos de los días, meses y años introducidos.*/
 
- public static void main(){}
+ public static void main(){
+  int dias_ano=365;
+  int horas_dia=24;
+  int minutos_hora=60;
+  int segundos_minuto=60;
+
+  int segundos_Ano= dias_ano*horas_dias*minutos_hora*segundos_minuto;
+  
+ }
 
